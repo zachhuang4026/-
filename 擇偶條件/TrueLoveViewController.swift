e0d845be03f809569ed82dac78cb05a9cebd07ac
“Zach” <bigrabbit716@gmail.com>
2017-05-08 17:22:55 +0800

    Outlet設定

---
+++ 擇偶條件/TrueLoveViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TrueLoveViewController.swift
+//  擇偶條件
+//
+//  Created by Zach on 2017/5/8.
+//  Copyright © 2017年 Zach. All rights reserved.
+//
+
+import UIKit
+
+class TrueLoveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var StarTextField: UITextField!
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
